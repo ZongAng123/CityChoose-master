11df86b6c68cae0e28a9ceab14def4b6a9485440
纵昂 <zongang@zongangdemacbook-pro.local>
2017-02-17 12:40:19 +0800

    Initial Commit

---
+++ CityChoose-master/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CityChoose-master
+//
+//  Created by 纵昂 on 2017/2/17.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
